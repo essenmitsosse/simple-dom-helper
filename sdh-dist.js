c2ef9db3a474c22242b3f723915f891a6cb110f1
marcus <marcus@essenmitsosse.de>
2017-09-13 16:48:34 +0200

    Add dist file build process

---
+++ sdh-dist.js
@@ -0,0 +1,5 @@
+window.domHelper = require( "./simple-dom-helper" );
+window.domHelper._elements = {
+	DomElement: require( "./lib/DomElement" ),
+	DomElementList: require( "./lib/DomElementList" )
+};
